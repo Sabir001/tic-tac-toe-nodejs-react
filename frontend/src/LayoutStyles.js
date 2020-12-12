c044c3e5cff514ef9dad671feb3fa9989adddb2a
sabir001 <asabir24@gmail.com>
2020-12-12 23:39:04 +0600

    Introducing styled component

---
+++ frontend/src/LayoutStyles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 960px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const TitleText = styled.h1`
+  text-align: center;
+`;
